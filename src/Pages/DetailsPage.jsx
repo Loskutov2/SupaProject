2a9a5b4831d68a11b9ba1becc22be7d3eb35dd32
Petro Zhylenko <petroz@ext.inditex.com>
2024-06-17 15:09:52 +0300

    TODO

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -1,7 +1,8 @@
 import { Details } from "components/Details/Details"
-
+//TODO:'notifications' is missing in props validation
 const DetailsPage = ({ notifications }) => {
     return(
+      //TODO: Redundant React fragments should be removed(must be <Details notifications={notifications}/> )
         <>
             <Details notifications={notifications}/>
         </>
